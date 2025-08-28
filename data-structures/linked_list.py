b99224b0a5bd68e8d9a17857cf4ad3eaaebcda19
Roma <romannityakin2006@gmail.com>
2025-08-28 19:18:05 +0300

    Create Linked List with is_empty and size properties

--- data-structures/linked_list.py
+++ data-structures/linked_list.py
@@ -7,3 +7,28 @@ class Node:
 
     def __repr__(self):
         return '<Node data: %s>' % self.data
+
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the numbers of nodes in the list
+        Takes O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
