f4f02ea1c2fd65f0a4e2a5999f4e636deccb2e74
Roma <romannityakin2006@gmail.com>
2025-08-28 19:30:07 +0300

    Add __repr__ and add methods to LinkedList object

--- data-structures/linked_list.py
+++ data-structures/linked_list.py
@@ -16,6 +16,26 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __repr__(self):
+        """
+        Returns a string representation of the list
+        Takes O(n) time
+        """
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+
+        return '-> '.join(nodes)
+
     def is_empty(self):
         return self.head == None
 
@@ -32,3 +52,12 @@ class LinkedList:
             current = current.next_node
 
         return count
+
+    def add(self, data):
+        """
+        Adds new Node containing data at head of the list
+        Takes O(1) time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
