86e2f9a5bd05440e993a0066977729800b62049c
Roma <romannityakin2006@gmail.com>
2025-08-28 19:45:13 +0300

    Add linear seacrh to the LinkedList object

--- data-structures/linked_list.py
+++ data-structures/linked_list.py
@@ -61,3 +61,21 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def seacrh(self, key):
+        """
+        Seacrh for the first node containing data that matches the key
+        Return the node or None if not found
+
+        Takes O(n) time
+        """
+
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+
+        return None
